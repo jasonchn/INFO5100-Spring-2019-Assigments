526002fd52b5fc3fce21e68809d6c6b60c4a1330
Jason Zhao <jasonchn@outlook.com>
2019-02-22 13:59:21 -0800

    update

--- Assignment_5/src/CheckOut.java
+++ Assignment_5/src/CheckOut.java
@@ -26,7 +26,9 @@ public class CheckOut {
     }
 
     public void clear(){
-        list.clear();
+
+        list.removeAll(list);
+
     }
 
     public int totalCost(){
@@ -39,11 +41,15 @@ public class CheckOut {
         System.out.println("\n" + "         "+ store.storeName);
         System.out.println("\n" + "       "+"-------------"+"\n");
 
+        if (list == null){
+            System.out.println();
+        }
+
         for (int i = 0; i < list.size(); i++){
             if (list.get(i) instanceof Candy){
-                receipt=  list.get(i).name + "           " + list.get(i).getCost()+ "\n" + ((Candy) list.get(i)).weight + "@" + ((Candy) list.get(i)).unitprice + "/lb." + "\n";
+                receipt=  list.get(i).name + "           " + list.get(i).getCost()+ "\n" + ((Candy) list.get(i)).weight + " "+ "@" + " "+ ((Candy) list.get(i)).unitprice + "/lb." + "\n";
             }else if (list.get(i) instanceof Cookies){
-                receipt +=  list.get(i).name + "           " + list.get(i).getCost() + "\n" + ((Cookies) list.get(i)).dozen + "@"+ ((Cookies) list.get(i)).unitprice + "/dz." + "\n";
+                receipt +=  list.get(i).name + "           " + list.get(i).getCost() + "\n" + ((Cookies) list.get(i)).dozen + " "+ "@"+" "+ ((Cookies) list.get(i)).unitprice + "/dz." + "\n";
             }else if (list.get(i) instanceof Sundae){
                 receipt += list.get(i).name + "           " + list.get(i).getCost() + "\n";
             }else if (list.get(i) instanceof  iceCream){
